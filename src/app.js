b165f0e7e1767e770ad1f9999a2f1918282bf1e5
Morgana Ximenes <morgana.ximenes@yahoo.ca>
2018-12-05 19:57:38 -0500

    WIP deploy to production

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 
-const routeConfig = require("./config/route-config.js");
+const appConfig = require("./config/main-config.js");
+ const routeConfig = require("./config/route-config.js");
 
+ appConfig.init();
  routeConfig.init(app);
 
 
 module.exports = app;
+
