5b2947f81dbd38192539ea6c58da5dffd8ec3de6
Morgana Ximenes <morgana.ximenes@yahoo.ca>
2018-12-23 19:04:10 -0500

    Checkpoint 8 completed

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-
 const postController = require("../controllers/postController")
 
 router.get("/topics/:topicId/posts/new", postController.new);
+router.post("/topics/:topicId/posts/create", postController.create);
 router.get("/topics/:topicId/posts/:id", postController.show);
 router.get("/topics/:topicId/posts/:id/edit", postController.edit);
 router.post("/topics/:topicId/posts/:id/update", postController.update);
