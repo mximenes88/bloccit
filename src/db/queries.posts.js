9124b68ea436718cdfed67974f3631480e41ef88
Morgana Ximenes <morgana.ximenes@yahoo.ca>
2018-12-26 12:38:39 -0500

    Assignment 8 post 2 fixed flair view

--- src/db/queries.posts.js
+++ src/db/queries.posts.js
@@ -1,56 +1,61 @@
-const Post = require("./models").Post;
-const Topic = require("./models").Topic;
+const Post = require('./models').Post;
+const Topic = require('./models').Topic;
+const Flair = require('./models').Flair;
+const postQueries = require('../db/queries.posts.js');
 
 module.exports = {
-    addPost(newPost, callback){
-        return Post.create(newPost)
-        .then((post) => {
-          callback(null, post);
-        })
-        .catch((err) => {
-          callback(err);
-        })
-      },
+	addPost(newPost, callback) {
+		return Post.create(newPost)
+			.then(post => {
+				callback(null, post);
+			})
+			.catch(err => {
+				callback(err);
+			});
+	},
+	getPost(id, callback) {
+		return Post.findById(id, {
+			include: [
+				{
+					model: Flair,
+					as: 'flairs',
+				},
+			],
+		})
+			.then(post => {
+				callback(null, post);
+			})
+			.catch(err => {
+				callback(err);
+			});
+	},
+	deletePost(id, callback) {
+		return Post.destroy({
+			where: { id },
+		})
+			.then(deletedRecordsCount => {
+				callback(null, deletedRecordsCount);
+			})
+			.catch(err => {
+				callback(err);
+			});
+	},
+	updatePost(id, updatedPost, callback) {
+		return Post.findById(id).then(post => {
+			if (!post) {
+				return callback('Post not found');
+			}
 
-      getPost(id, callback){
-        return Post.findById(id)
-        .then((post) => {
-          callback(null, post);
-        })
-        .catch((err) => {
-          callback(err);
-        })
-      },
-
-      deletePost(id, callback){
-        return Post.destroy({
-          where: { id }
-        })
-        .then((deletedRecordsCount) => {
-          callback(null, deletedRecordsCount);
-        })
-        .catch((err) => {
-          callback(err);
-        })
-      },
-
-      updatePost(id, updatedPost, callback){
-        return Post.findById(id)
-        .then((post) => {
-          if(!post){
-            return callback("Post not found");
-          }
-   
-          post.update(updatedPost, {
-            fields: Object.keys(updatedPost)
-          })
-          .then(() => {
-            callback(null, post);
-          })
-          .catch((err) => {
-            callback(err);
-          });
-        });
-      }
-
-}
+			post
+				.update(updatedPost, {
+					fields: Object.keys(updatedPost),
+				})
+				.then(() => {
+					callback(null, post);
+				})
+				.catch(err => {
+					callback(err);
+				});
+		});
+	},
+};
--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -1,75 +1,80 @@
-const Topic = require ("./models").Topic;
-const Post = require("./models").Post;
-module.exports ={
-    getAllTopics(callback){
-        return Topic.all()
-        .then((topics)=>{
-            callback(null,topics);
-        })
-        .catch((err)=>{
-            callback(err);
-        })
-    },
+const Topic = require('./models').Topic;
+const Post = require('./models').Post;
+const Flair = require('./models').Flair;
 
-    getTopic(id, callback){
-        return Topic.findById(id,{
-            include: [{
-                model: Post,
-                as:"posts"
-            }]
+module.exports = {
+	getAllTopics(callback) {
+		return Topic.all()
+			.then(topics => {
+				callback(null, topics);
+			})
+			.catch(err => {
+				callback(err);
+			});
+	},
 
-        })
-        .then((topic) => {
-          callback(null, topic);
-        })
-        .catch((err) => {
-          callback(err);
-        })
-      },
+	addTopic(newTopic, callback) {
+		return Topic.create({
+			title: newTopic.title,
+			description: newTopic.description,
+		})
+			.then(topic => {
+				callback(null, topic);
+			})
+			.catch(err => {
+				callback(err);
+			});
+	},
 
-    addTopic(newTopic, callback){
-        return Topic.create({
-            title: newTopic.title,
-            description: newTopic.description
-        })
-        .then((topic) => {
-            callback(null, topic);
-          })
-          .catch((err) => {
-            callback(err);
-          })
-    },
+	getTopic(id, callback) {
+		return Topic.findById(id, {
+			include: [
+				{
+					model: Post,
+					as: 'posts',
+				},
+				{
+          model: Flair,
+          as: "flairs"
+        },
+			],
+		})
+			.then(topic => {
+				callback(null, topic);
+			})
+			.catch(err => {
+				callback(err);
+			});
+	},
 
-     deleteTopic(id, callback){
-         return Topic.destroy({
-             where: {id}
-         })
-         .then((topic) =>{
-            callback(null,topic);
-         })
-         .catch((err) =>{
-            callback(err);
-         })
-     },
+	deleteTopic(id, callback) {
+		return Topic.destroy({
+			where: { id },
+		})
+			.then(topic => {
+				callback(null, topic);
+			})
+			.catch(err => {
+				callback(err);
+			});
+	},
 
-     updateTopic(id, updatedTopic, callback){
-         return Topic.findById(id)
-         .then((topic) =>{
-             if(!topic){
-                 return callback("Topic not found");
-             }
+	updateTopic(id, updatedTopic, callback) {
+		return Topic.findById(id).then(topic => {
+			if (!topic) {
+				return callback('Topic not found');
+			}
 
-             topic.update(updatedTopic, {
-                 fields: Object.keys(updatedTopic)
-             })
-             .then(()=>{
-                 callback(null,topic);
-             })
-             .catch((err) =>{
-                 callback(err);
-             })
-         })
-     }
-}
-
-
+			topic
+				.update(updatedTopic, {
+					fields: Object.keys(updatedTopic),
+				})
+				.then(() => {
+					callback(null, topic);
+				})
+				.catch(err => {
+					callback(err);
+				});
+		});
+	},
+};
